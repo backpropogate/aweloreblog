5be6df5a1d86670c0dd3f5e99242395002dbd833
wil <Workoutwwill@gmail.com>
2022-03-16 16:55:45 -0400

    change marquee text

--- components/Marquee.js
+++ components/Marquee.js
@@ -5,7 +5,7 @@ const Marquee = () => {
     
   return (
     <div className='bg-black w-full h-7 flex justify-around marquee'>
-        <h2 className='text-white'>Technology Reviews You Can Really On</h2>
+        <h2 className='text-white'>Technology Reviews You Can Rely On</h2>
       
         </div>
   )
