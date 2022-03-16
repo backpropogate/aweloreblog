014cd59478044a2158d3227e927bc9f2016d03c3
wil <Workoutwwill@gmail.com>
2022-03-16 17:59:39 -0400

    homepage

--- components/Main.js
+++ components/Main.js
@@ -1,34 +1,47 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Link from 'next/link'
+import client from '../client'
 
 
 
 const Main = ({posts, categories, authors}) => {
+  const [postData, setPost] = useState();
+  useEffect(() => {
+    client.fetch(
+        `*[_type == "post"]{
+            title,
+            slug,
+            author,
+            mainImage{
+                asset->{
+                    _id,
+                    url,
+                },
+                alt
+            },
+        }`
+    )
+    .then((data) => {
+        setPost(data)} )  
+    .catch(console.error)
+},[])
     
   return (
     <div>
        
         <h1 className='font-bold text-center mt-10 mb-10 text-2xl sm:text-4xl latestArticles'>Latest Articles</h1>
-        <div className='flex p-5 gap-10'>
-            <Link  href="/blog">
-            <div className='rounded-md cursor-pointer hover:opacity-60'>
-            <img className=" rounded-md"src="/biz.jpg" alt="" /> 
-            <h2 className='font-bold'>5 ways to blah blah</h2>
-            <p className='text-gray-500 italic'>Published: March, 2021</p>
-            </div>
-            </Link>
-            <div className='rounded-md cursor-pointer hover:opacity-60'>
-            <img className='rounded-md' src="/biz.jpg" alt="" /> 
-            <h2 className='font-bold'>5 ways to blah blah</h2>
-            <p className='text-gray-500 italic'>Published: March, 2021</p>
-            </div>
-        </div>
+        
+       <div className='grid grid-cols-2 sm:grid-cols-3'>
+        {postData && postData.map((post, index ) =>(
         <div className='w-full rounded-md p-5 cursor-pointer hover:opacity-60'>
-        <img className='rounded-md mb-5' src="/biz.jpg" alt="" /> 
-        <h2 className='font-bold text-2xl'>5 ways to blah blah</h2>
+        <img className='rounded-md mb-5' src={post.mainImage.asset.url} alt="" /> 
+        <h2 className='font-bold text-2xl'>{post.title}</h2>
         <p className='text-gray-500 italic text-md'>Published: March, 2021</p>
-
+      
         </div>
+          ))}
+          </div>
+      
     </div>
   )
 }
